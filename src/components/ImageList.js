ca6127d58ce3bd7cf9cce09dfe6a20d429d71e98
EugeneButovka <eugene.butovka@gmail.com>
2019-07-01 15:30:27 +0300

    add auto grid fitting image

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import './ImageList.css'
+import ImageCard from './ImageCard'
 
 const ImageList = (props) => {
-    /*straightforward*/
-    /*
-    const images = props.images.map((image) => {
-        return <img
-            key={image.id}
-            src={image.urls.regular}
-            alt={image.description}
-        />
-    });
-    */
-
     /*with destructive syntax*/
-    const images = props.images.map(({ description, id, urls }) => {
-        return <img
-            key={id}
-            src={urls.regular}
-            alt={description}
+    const images = props.images.map((image) => {
+        return <ImageCard
+            key={image.id}
+            image={image}
         />
     });
 
